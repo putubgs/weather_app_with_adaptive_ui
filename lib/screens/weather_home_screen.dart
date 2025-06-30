c2279956ed2f21ee3dad69a72bf31ddea739b621
SeedONova <putubaguswidia@gmail.com>
2025-06-30 20:15:00 +0700

    feat: adding bottom navigation bar

--- lib/screens/weather_home_screen.dart
+++ lib/screens/weather_home_screen.dart
@@ -15,13 +15,6 @@ class WeatherHomeScreen extends StatefulWidget {
 class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
   List<WeatherModel> weatherList = [];
   bool isLoading = true;
-  int _selectedIndex = 0;
-
-  static const List<Widget> _pages = [
-    Center(child: Text('Home')),
-    Center(child: Text('Search')),
-    Center(child: Text('Profile')),
-  ];
 
   @override
   void initState() {
@@ -38,35 +31,19 @@ class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
     });
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  void _navigateToDetail() {
-    Navigator.pushNamed(context, '/detail', arguments: "Hello from Home!");
-  }
+  // void _navigateToDetail() {
+  //   Navigator.pushNamed(context, '/detail', arguments: "Hello from Home!");
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
       body: isLoading ? _buildLoadingWidget() : _buildWeatherContent(),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.deepPurple,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _navigateToDetail,
-        child: Icon(Icons.navigate_next),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _navigateToDetail,
+      //   child: Icon(Icons.navigate_next),
+      // ),
     );
   }
 
@@ -76,7 +53,7 @@ class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
 
     return AppBar(
       title: Text(
-        'Weather App',
+        'Weather Lists',
         style: TextStyle(
           fontSize: isLargeScreen
               ? AppConstants.fontXLarge
---
+++ lib/widgets/bottom_navigation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../screens/weather_home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<BottomNavBar> {
+  int _selectedIndex = 1;
+
+  static const List<Widget> _pages = <Widget>[
+    WeatherHomeScreen(),
+    Text('Index 0: Pick Weather'),
+    Text('Index 2: Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Current Weather')),
+      body: Center(child: _pages.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
